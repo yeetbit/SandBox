341e5ad997e51f963d1c2a445109f69569b7d317
seb <git@00b.it>
2021-05-10 10:25:06 +0200

    .

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,12 +4,17 @@ import testing.*;
 
 public class Main{
 
+	static String testparse = "ueaue1236";
+	static int yeetmode;
+
 
     
 
     public static void main (String[] args) {
 
-
+		// trying to see if parse int
+		yeetmode = Integer.parseInt(testparse, 10); // wont compile
+		System.out.println(yeetmode);
         new StreamTests();
 
     }
@@ -17,4 +22,4 @@ public class Main{
  
 
 }
-
+<
