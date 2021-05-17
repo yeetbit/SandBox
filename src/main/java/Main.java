c7e1fb826a4227d7a6c8ea6307e8b5be23704fbb
seb <git@00b.it>
2021-05-17 11:14:49 +0200

    .

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,30 @@
 
-
 import testing.*;
 
 public class Main{
 
-	static String testparse = "aou1236";
-	static int yeetmode;
-
-
-    
-
     public static void main (String[] args) {
 
-		// trying to see if parses decimal string 
-		yeetmode = Integer.parseInt(testparse, 10); // wont compile
-		System.out.println(yeetmode);
-        new StreamTests();
+		int n1 = 4;
+		int n2 = 4;
+
+
+		boolean b1 = n1==n2;
+
+		long exp = power(n1, n2);
+        // new StreamTests();
+		System.out.println(n1+" at the power of "+n2+" = "+exp);
 
     }
 
- 
+	public static long power(int num, int pow){
 
+		var temp = num;
+		for(var i=1;i<pow;i++){
+			temp*=num;
+		}
+		return temp;
+
+	}
 }
 
