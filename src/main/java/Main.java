68201fa5ff7cac4c08c6f98254aa46b5f65c88a6
seb <git@00b.it>
2021-05-18 14:04:00 +0200

    .

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,15 +6,16 @@ public class Main{
     public static void main (String[] args) {
 
 		int n1 = 4;
-		int n2 = 4;
+		int n3 = 4;
+		float f1 = n3;
+		n3 = (int) f1;
+		boolean b1 = n1==n3;
 
-
-		boolean b1 = n1==n2;
-
-		long exp = power(n1, n2);
         // new StreamTests();
-		System.out.println(n1+" at the power of "+n2+" = "+exp);
-
+		// System.out.println(n1+" at the power of "+n2+" = "+exp);
+		System.out.println("You can up-cast automatically from int to float "+ f1);
+		System.out.println("You can down-cast from float to int "+ n3);
+		System.out.println("\nEven while they are the same width");
     }
 
 	public static long power(int num, int pow){
