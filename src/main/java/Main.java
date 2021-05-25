15100326b29a3ace6b319872e8da51309f6e26d0
seb <git@00b.it>
2021-05-25 21:50:06 +0200

    .

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,23 +2,17 @@ import sandbox.testing.instantiableObjects.*;
 
 public class Main{
 
-    public static void main (String[] args) {
+    public static void main (String[] args) throws Error {
 
 		if(args.length !=1 || args.length >1){
-			System.err.println("Usage: sandbox Time of year tool\n    sandbox -w 20 // weeknumber\n    sandbox -d 150 // daynumber\n    cheers");
-
-			
-
-    }
-
-	public static long power(int num, int pow){
-
-		var temp = num;
-		for(var i=1;i<pow;i++){
-			temp*=num;
+			System.err.println("Usage: sandbox Time of yAear tool\n    sandbox -w 20 // weeknumber\n    sandbox -d 150 // daynumber\n    cheers");
+			System.out.
+			System.exit(-1);
+		}else {
+			TimeOfYear toy = new TimeOfYear();
+			String msg = toy.timeOfYear(args[0]);
+			System.out.println(msg);
 		}
-		return temp;
-
-	}
+    }
 }
 
--- src/main/java/sandbox/testing/TimeOfYear.java
+++ src/main/java/sandbox/testing/TimeOfYear.java
@@ -2,9 +2,14 @@ package testing;
 
 public class TimeOfYear {
 
-	public TimeOfYear(){
-
-
-
-
+	public String timeOfYear(String in){
+		int useableIn = -1;
+		try{
+			useableIn = Integer.parseInt(in.trim());
+			return new StringBuilder(useableIn);
+					
+		}catch(Exception e){
+			return "bad input"+e;
+		}
+	}
 }
